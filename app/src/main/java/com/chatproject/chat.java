7ca6b32ff20d2361a812a4aaa75205174b7220c1
Tomasz Ciechanowski <tomaszciechanowski@wenklystudio.com>
2021-12-19 15:21:08 +0100

    upload project

---
+++ app/src/main/java/com/chatproject/chat.java
@@ -0,0 +1,14 @@
+package com.chatproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat);
+    }
+}
